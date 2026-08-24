bf9812db9d2d714c6f9419def54dab9f092722a3
lutetium595-bit <lutetium595@gmail.com>
2026-08-24 14:28:26 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ const ALLOWED_KEYS = new Set([
   "skyCalendarEvents",
   "sky_notice_data",
   "skyDresses",
-  "skyUpboItems"
+  "skyUpboItems",
+  "skyProfileData"
 ]);
 
 app.use(express.json({ limit: "5mb" }));
